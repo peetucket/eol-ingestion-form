9fb4dc3ec2a0d532d91c2d80885b5581b851fa2e
peetucket <peetucket@a2dd967e-083a-0410-ad1c-7105d7433e3e>
2007-09-26 20:13:23 +0000

    consolidate views; created nested controller for user admin controller to allow for future admin functions in other controllers

--- app/controllers/admin/user_controller.rb
+++ app/controllers/admin/user_controller.rb
@@ -1,4 +1,4 @@
-class AdminController < ApplicationController
+class Admin::UserController < ApplicationController
 
     before_filter :check_authentication_admin
     verify :method => :post, :only => [ :destroy_user ],
